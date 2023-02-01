e2844d02e662ccf0c2d333dd39d2ec5c062e276b
ZuuZaa <zumbaghirova@gmail.com>
2023-02-01 14:32:09 +0400

    price boxes changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,15 @@
 import { cars, cities, banType, years } from "./utils.js";
+import { data } from "./data.js";
 
 const selectBoxes = document.querySelectorAll(".select");
-
-const dataForFilter = {
-    // brand: null,
-    // model: null,
-    // driven: null,
-    // cityName: null,
-    // maxPrice: null,
-    // minPrice: null,
-    // currency: null,
-    // credit: null,
-    // barter: null,
-    // banType: null,
-    // minYear: null,
-    // maxYear: null
-};
+const creditChekbox = document.querySelector("#credit-checkbox");
+const barterChekbox = document.querySelector("#barter-checkbox");
 
 const selected = (id, toggle = false) => {
     selectBoxes.forEach((item) => {
         if (item.querySelector(".select-input").value && !item.classList.contains("not-selected")) item.classList.add("selected");
         else {
+            //console.log("else", id,item.querySelector(".select-input").value)
             if (item.id == id) {
                 if (toggle) {
                     item.classList.toggle("selected");
@@ -31,13 +20,13 @@ const selected = (id, toggle = false) => {
                 item.classList.remove("selected");
             }
         }
+        !item.querySelector(".select-input").value && item.classList.remove("typing")
     });
 };
 
 const open = (id, toggle) => {
     selectBoxes.forEach((item) => {
         if (item.id == id) {
-            // item.querySelector(".select-input").focus()
             if (toggle) {
                 item.classList.toggle("open");
                 item.querySelector(".chevron").classList.toggle("rotated");
@@ -48,165 +37,234 @@ const open = (id, toggle) => {
         } else {
             item.classList.remove("open");
             item.querySelector(".chevron").classList.remove("rotated");
-            // item.querySelector(".select-input").blur()
 
         }
     });
 };
 
 const resetItem = (box) => {
+    box.querySelector(".dropdown").innerHTML = "";
     const listItem = document.createElement("li");
     const crossIcon = document.createElement("i");
     crossIcon.classList.add("fa-solid", "fa-xmark");
-    const resetSpan = document.createElement("span");
-    resetSpan.textContent = "Sıfırla";
+    listItem.textContent = "Sıfırla";
     listItem.classList.add("reset-dropdown");
-    listItem.append(crossIcon, resetSpan);
+    listItem.prepend(crossIcon);
     listItem.addEventListener("click", () => {
         selected(box.id);
         box.querySelector(".select-input").value = "";
+        box.id == "brand-select" && disabledModelSelect();
     })
-    return listItem;
+    box.querySelector(".dropdown").append(listItem);
 };
 
-const dropdownRender = (list, selectBox, key) => {
-    const container = selectBox.querySelector(".dropdown");
-    container.innerHTML = "";
-
-    if (list.length > 0) {
-        container.append(resetItem(selectBox));
-        list.map((item) => {
-            const listItem = document.createElement("li");
-            listItem.textContent = (key == "minYear" || key == "maxYear") ? item.year : item[key];
-            (key == "minYear" || key == "maxYear")
-                ? listItem.classList.add(item.year == dataForFilter[key] ? "selected-option" : "dropdown-option")
-                : listItem.classList.add(item[key] == dataForFilter[key] ? "selected-option" : "dropdown-option");
-            listItem.addEventListener("click", (e) => {
-                dataForFilter[key] = e.target.textContent;
-                selectBox.querySelector(".select-input").value = e.target.textContent;
-                console.log(selectBox.classList)
-                if (key == "brand") {
-                    document.querySelector("#model-select").classList.remove("disabled");
-                    document.querySelector("#model-select .select-input").removeAttribute("disabled");
-                    document.querySelector("#model").value = "";
-                }
-            })
-            container.append(listItem);
-        });
+const disabledModelSelect = () => {
+    const trigger = document.querySelector("#brand-select");
+    const target = document.querySelector("#model-select");
+    if (trigger.querySelector(".select-input").value) {
+        target.classList.remove("disabled");
+        target.querySelector(".select-input").removeAttribute("disabled");
     } else {
-        const listItem = document.createElement("li");
-        listItem.textContent = "Tapılmadı"
-        container.append(listItem);
+        target.classList.add("disabled");
+        target.querySelector(".select-input").setAttribute("disabled", "");
     }
-
-};
-
-
-const modelsSelector = (filter = false) => {
-    if (dataForFilter.brand) {
-        document.querySelector("#model-dropdown").innerHTML = "";
-        document.querySelector("#model-dropdown").append(resetItem(document.querySelector("#model-select")));
-        const arr = cars.find(car => car.brand == dataForFilter.brand).models;
-        const models = filter ? arr.filter(item => item.toLocaleLowerCase().includes(filter)) : arr;
-        models.map(model => {
-            const listItem = document.createElement("li");
-            listItem.classList.add(model == dataForFilter.model ? "selected-option" : "dropdown-option");
-            const label = document.createElement("label");
-            label.for = model;
-            label.textContent = model;
-            const checkbox = document.createElement("input");
-            checkbox.setAttribute("type", "checkbox");
-            checkbox.classList.add("model-checkbox");
-            checkbox.id = model;
-            checkbox.value = model;
-            if (checkbox.value == dataForFilter.model) checkbox.setAttribute("checked", "checked")
-            listItem.append(label, checkbox);
-            listItem.addEventListener("click", () => {
-                document.querySelector("#model").value = model;
-                dataForFilter.model = model
-            })
-            document.querySelector("#model-dropdown").append(listItem)
-        })
-    }
+    target.querySelector(".select-input").value = "";
 }
 
+const radioListItem = (box, item, key) => {
+
+    const content = key === "brand" ? item[key] : item;
+    const text = `${key}-${content.toLocaleLowerCase().replace(" ", "-")}`
+
+    const listItem = document.createElement("li");
+    const label = document.createElement("label");
+    label.setAttribute("for", text);
+    label.textContent = content;
+
+    const radioInput = document.createElement("input");
+    radioInput.id = text;
+    radioInput.setAttribute("type", "radio");
+    radioInput.value = content;
+    radioInput.checked = box.querySelector(".select-input").value == content
+    radioInput.name = key;
+
+    listItem.addEventListener("click", (e) => {
+        e.stopPropagation()
+        box.querySelector(`#${key}`).value = content
+        box.classList.remove("open");
+        if (key === "brand") {
+            const modelSelect = document.querySelector("#model-select")
+            const models = cars.find(car => car[key] == item[key]).models
+            disabledModelSelect();
+            resetItem(modelSelect);
+            models.forEach(
+                model => modelSelect.querySelector(".dropdown").append(checkboxListItem(modelSelect, model))
+            )
+        }
+    })
+    listItem.append(label, radioInput);
+    return listItem;
+}
+
+const checkboxListItem = (box, item) => {
+
+    const listItem = document.createElement("li");
+    listItem.classList.add("select-checkbox");
+
+    const label = document.createElement("label");
+    label.setAttribute("for", item.toLocaleLowerCase().replace(" ", "-"));
+    label.classList.add("checkbox-label");
+    label.textContent = item;
+
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.id = item.toLocaleLowerCase().replace(" ", "-");
+    checkbox.setAttribute("value", item);
+    checkbox.checked = box.querySelector(".select-input").value.includes(item);
+
+    listItem.addEventListener("click", (e) => {
+        const checkedArr = []
+        e.stopPropagation();
+        box.querySelectorAll("input[type=checkbox]:checked").forEach(inp => checkedArr.push(inp.value))
+        box.querySelector(".select-input").value = checkedArr.join(", ");
+    })
+    listItem.append(label, checkbox);
+
+    return listItem
+}
+
+const checkboxButton = (box) => {
+    box.querySelector("input[type=checkbox]").checked
+        ? box.classList.add("selected-checkbox")
+        : box.classList.remove("selected-checkbox");
+}
+
+// const dropdownRender = (list, selectBox, key) => {
+//     const container = selectBox.querySelector(".dropdown");
+//     container.innerHTML = "";
+
+//     if (list.length > 0) {
+//         container.append(resetItem(selectBox));
+//         list.map((item) => {
+//             const listItem = document.createElement("li");
+//             listItem.textContent = (key == "minYear" || key == "maxYear") ? item.year : item[key];
+//             (key == "minYear" || key == "maxYear")
+//                 ? listItem.classList.add(item.year == dataForFilter[key] ? "selected-option" : "dropdown-option")
+//                 : listItem.classList.add(item[key] == dataForFilter[key] ? "selected-option" : "dropdown-option");
+//             listItem.addEventListener("click", (e) => {
+//                 dataForFilter[key] = e.target.textContent;
+//                 selectBox.querySelector(".select-input").value = e.target.textContent;
+//                 if (key == "brand") {
+//                     document.querySelector("#model-select").classList.remove("disabled");
+//                     document.querySelector("#model-select .select-input").removeAttribute("disabled");
+//                     document.querySelector("#model").value = "";
+//                 }
+//             })
+//             container.append(listItem);
+//         });
+//     } else {
+//         const listItem = document.createElement("li");
+//         listItem.textContent = "Tapılmadı"
+//         container.append(listItem);
+//     }
+
+// };
 
 const dropdownToggleHandler = (box, toogle) => {
-    if (!box.classList.contains("disabled")) {
-        selected(box.id, toogle);
-        open(box.id, toogle);
-    }
+
     switch (box.id) {
         case "brand-select":
-            dropdownRender(cars, box, "brand");
+            resetItem(box);
+            cars.forEach(car => box.querySelector(".dropdown").append(radioListItem(box, car, "brand")))
             break;
         case "city-select":
-            dropdownRender(cities, box, "cityName");
+            resetItem(box);
+            cities.forEach(city => box.querySelector(".dropdown").append(checkboxListItem(box, city)))
             break;
         case "model-select":
-            modelsSelector();
+            resetItem(box);
+            const brand = document.querySelector("#brand").value;
+            const models = cars.find(car => car.brand == brand)?.models
+            models?.forEach(model => box.querySelector(".dropdown").append(checkboxListItem(box, model)))
             break
         case "ban-select":
-            dropdownRender(banType, box, "banType");
+            resetItem(box);
+            banType.forEach(ban => box.querySelector(".dropdown").append(checkboxListItem(box, ban)))
             break;
         case "min-year-select":
-            dropdownRender(years, box, "minYear");
+            resetItem(box);
+            years.reverse().forEach(year => box.querySelector(".dropdown").append(radioListItem(box, year, "min-year")))
             break
         case "max-year-select":
-            dropdownRender(years, box, "maxYear");
+
+            resetItem(box);
+            years.forEach(year => box.querySelector(".dropdown").append(radioListItem(box, year, "max-year")))
             break
+        case "currency-select":
+            box.querySelector("#currency").value = box.querySelector("input:checked").value;
+            
+            box.querySelector(".dropdown").addEventListener("click", () => box.classList.remove("not-selected"))
         default:
             console.log("default");
             break;
     }
+    if (!box.classList.contains("disabled")) {
+        selected(box.id, toogle);
+        open(box.id, toogle);
+    }
 }
-
 
 const formReset = form => {
     form.reset();
-    selectBoxes.forEach(box=> {
-        box.classList.remove("selected")
+    selectBoxes.forEach(box => {
+        box.id = "currency-select" && box.classList.add("not-selected")
+        selected(box.id);
+        open(box.id)
     })
+
+    const dataForFilter = {};
     Object.keys(dataForFilter).forEach(key => {
         delete dataForFilter[key];
-      });
-      document.querySelector("#model-select").classList.add("disabled");
-      document.querySelector("#model-select .select-input").setAttribute("disabled", "")
-      document.querySelector("#model-select .chevron").classList.remove("rotated");
-      console.log(dataForFilter)
+    });
+
+    console.log(document.querySelector("#model-select"))
+   // document.querySelector("#model-select").classList.add("disabled");
+    document.querySelector("#model").setAttribute("disabled", "");
+    document.querySelector("#credit-checkbox").classList.remove("selected-checkbox");
+    document.querySelector("#barter-checkbox").classList.remove("selected-checkbox");
+
+    console.log(dataForFilter)
 }
+
+
 
 selectBoxes.forEach(box => {
 
     box.addEventListener("input", () => {
-        const inputValue = box.querySelector(".select-input").value.trim().toLocaleLowerCase();
-        switch (box.id) {
-            case "brand-select":
-                const filteredCars = cars.filter(car => car.brand.toLocaleLowerCase().startsWith(inputValue))
-                dropdownRender(filteredCars, box, "brand")
-                break;
-            case "city-select":
-                const filteredCities = cities.filter(item => item.cityName.toLocaleLowerCase().startsWith(inputValue))
-                dropdownRender(filteredCities, box, "cityName")
-                break;
-            case "model-select":
-                modelsSelector(inputValue.toLocaleLowerCase());
-                break;
-            default:
-                console.log("default");
-                break;
-        }
+        box.classList.add("typing");
+
+
+        //const inputValue = box.querySelector(".select-input").value.trim().toLocaleLowerCase();
+        // switch (box.id) {
+        //     case "brand-select":
+        //         const filteredCars = cars.filter(car => car.brand.toLocaleLowerCase().startsWith(inputValue))
+        //         dropdownRender(filteredCars, box, "brand")
+        //         break;
+        //     case "city-select":
+        //         const filteredCities = cities.filter(item => item.cityName.toLocaleLowerCase().startsWith(inputValue))
+        //         dropdownRender(filteredCities, box, "cityName")
+        //         break;
+        //     case "model-select":
+        //         modelsSelector(inputValue.toLocaleLowerCase());
+        //         break;
+        //     default:
+        //         console.log("default");
+        //         break;
+        // }
     });
 
     box.addEventListener("click", (e) => {
         dropdownToggleHandler(box, false)
-        if (e.target.tagName == "LI" || e.target.tagName == "LABEL" || e.target.classList.contains("model-checkbox") && !e.target.classList.contains("reset-dropdown")) box.classList.remove("open");
-
-        if (box.id == "currency-select") {
-            dataForFilter.currency = box.querySelector(".select-input").value;
-        }
-        console.log(dataForFilter)
-
     });
 
     box.querySelector(".chevron").addEventListener("click", (e) => {
@@ -215,35 +273,29 @@ selectBoxes.forEach(box => {
     });
 });
 
-document.querySelectorAll("#currency-select ul li").forEach(item => item.addEventListener("click", (e) => {
-    document.querySelector("#currency-select > .select-input").value = e.target.textContent;
-    document.querySelector("#currency-select").classList.remove("not-selected")
-}))
-
-document.querySelectorAll(".radio").forEach(btn => {
-    btn.addEventListener("click", e => dataForFilter.driven = e.target.control.value);
-})
-
+document.querySelector("#min-price").addEventListener(
+    "click",()=> document.querySelector("#min-price").classList.add("selected", "typing")
+)
+barterChekbox.addEventListener("click", () => checkboxButton(barterChekbox))
+creditChekbox.addEventListener("click", () => checkboxButton(creditChekbox))
 
 const filteringForm = document.querySelector(".filtering-form")
-filteringForm.addEventListener("submit", (e)=>{
+filteringForm.addEventListener("submit", (e) => {
+
     e.preventDefault();
-    
-    // fieldsForFilter.brand = brandInput.value
-    console.log("submit", dataForFilter);
+
+    let filteredArr = [];
+    Object.keys(dataForFilter).forEach(key => {
+        filteredArr = [...filteredArr, ...data.filter(car => car[key] == dataForFilter[key])]
+    })
+    new Set(filteredArr).forEach(
+        item => console.log(item)
+    )
+
     formReset(e.target)
 })
 
-filteringForm.addEventListener("reset", (e)=>{
+filteringForm.addEventListener("reset", (e) => {
     formReset(e.target)
 })
 
-
-// // brandSelect.addEventListener("click", (e)=>{
-// //     document.querySelector(".placeholder").textContent = "Markanı yazın"
-// // })
-
-
-// brandSelect.addEventListener("input", ()=>{
-//     document.querySelector(".placeholder").textContent = "Marka"
-// })
